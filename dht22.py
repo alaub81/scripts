52799f6af6cf3bc03ea9b9ef16ae52a75883704e
root <root@vs.laub-home.de>
2020-06-17 12:08:21 +0200

    Added dht22.py

---
+++ dht22.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import time
+import board
+import adafruit_dht
+
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT22(board.D4)
+
+def main():
+        # Print the values to the serial port
+        temperature_c = dhtDevice.temperature
+        humidity = dhtDevice.humidity
+        print(
+            "Temp: {:.1f} C    Humidity: {}% ".format(
+                temperature_c, humidity
+            )
+        )
+
+if __name__=="__main__":
+   main()
