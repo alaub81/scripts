93dd83cfe0bad7cd1060b73db560b0a91bb7598b
Andreas Laub <andreas@laub-home.de>
2025-07-09 18:52:26 +0200

    new version

--- wlan-check.sh
+++ wlan-check.sh
@@ -1,37 +1,58 @@
 #!/usr/bin/env bash
 #########################################################################
-#Name: wlan-check.sh
-#Subscription: This Script does a inerface restart, 
-#if the gateway is not reachable
-##by A. Laub
-#andreas[-at-]laub-home.de
-#
-#License:
-#This program is free software: you can redistribute it and/or modify it
-#under the terms of the GNU General Public License as published by the
-#Free Software Foundation, either version 3 of the License, or (at your option)
-#any later version.
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-#or FITNESS FOR A PARTICULAR PURPOSE.
+# Name: wlan-check.sh
+# Purpose: Reconnects WLAN if connection to gateway is lost
+# Author: A. Laub - andreas[-at-]laub-home.de
 #########################################################################
-#Set the language
+
+# Sprache & Umgebungsvariablen
 export LANG="en_US.UTF-8"
-#Load the Pathes
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-# set the variables
 
-GATEWAY="192.168.50.1"
-NETWORKINTERFACE="wlan0"
+# Konfiguration
+GATEWAY="192.168.60.1"
+LOGFILE="/root/wlancheck.log"
+TIMESTAMP=$(date -Is)
 
-### Do the stuff
-ping -w 30 -c 1 $GATEWAY > /dev/null 2>&1
+# Richtiges Interface ermitteln (ignoriere p2p-Devices)
+WLAN_IFACE=$(iw dev | awk '$1=="Interface"{print $2}' | grep -v '^p2p-' | head -n1)
+
+# Falls kein Interface erkannt wurde – Abbruch mit Log
+if [ -z "$WLAN_IFACE" ]; then
+    echo "$TIMESTAMP Kein gültiges WLAN-Interface gefunden!" >> "$LOGFILE"
+    exit 1
+fi
+
+# Prüfe, ob mit WLAN verbunden (SSID vorhanden)
+SSID=$(iwgetid "$WLAN_IFACE" --raw)
+
+if [ -z "$SSID" ]; then
+    echo "$TIMESTAMP Keine SSID verbunden." >> "$LOGFILE"
+else
+    echo "$TIMESTAMP Verbunden mit SSID: $SSID" >> "$LOGFILE"
+fi
+
+# Prüfe Verbindung zum Gateway
+ping -I "$WLAN_IFACE" -c 1 -W 5 "$GATEWAY" > /dev/null 2>&1
+
 if [ $? -ne 0 ]; then
-        echo "Restart $NETWORKINTERFACE"
-        ip link set $NETWORKINTERFACE down
-        ip link set $NETWORKINTERFACE up
-        # deprecated
-        #ifdown $NETWORKINTERFACE
-        #ifup $NETWORKINTERFACE
+    echo "$TIMESTAMP Gateway nicht erreichbar. Reconnect wird durchgeführt..." >> "$LOGFILE"
+
+    # WLAN trennen
+    ip link set "$WLAN_IFACE" down
+    sleep 2
+    ip link set "$WLAN_IFACE" up
+    sleep 2
+
+    # Konfiguration neu laden
+    wpa_cli -i "$WLAN_IFACE" reconfigure >> "$LOGFILE" 2>&1
+    sleep 3
+
+    # Neue IP anfordern
+    dhcpcd -n "$WLAN_IFACE" >> "$LOGFILE" 2>&1
+
+    echo "$TIMESTAMP Reconnect-Versuch abgeschlossen." >> "$LOGFILE"
+else
+    echo "$TIMESTAMP Verbindung zum Gateway OK." >> "$LOGFILE"
 fi
 
